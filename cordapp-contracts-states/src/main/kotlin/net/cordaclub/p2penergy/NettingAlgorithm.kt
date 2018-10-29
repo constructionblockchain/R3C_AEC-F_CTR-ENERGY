276c6f087b9c4326198f4f6b6383e060a3630dc8
Tudor Malene <tudor.malene@gmail.com>
2018-10-29 20:19:06 +0000

    Add simple main function

--- cordapp-contracts-states/src/main/kotlin/net/cordaclub/p2penergy/NettingAlgorithm.kt
+++ cordapp-contracts-states/src/main/kotlin/net/cordaclub/p2penergy/NettingAlgorithm.kt
@@ -3,15 +3,31 @@ package net.cordaclub.p2penergy
 import net.corda.core.identity.Party
 
 //data class NettingResult(val amountToPay: Amount<Currency>?, val amountToReceive: Amount<Currency>?)
-data class NettingResult(val amountToPay: Int?, val amountToReceive: Int?)
+data class NettingResult(val amountToPay: Int? = null, val amountToReceive: Int? = null)
 
 // pricing
-fun netting(readings: List<EnergyEventState>, utilityCompany: Party): Map<Party, NettingResult> {
+//receives a map of PartyName-readingValue
+fun netting(readings: Map<String, Int>, utilityCompany: String): Map<String, NettingResult> {
 //TODO
 // 1) sum up all readings (reading =end-start)
 // 2) if sum >0 - participants who produced need to receive, etc
 // 3) calculate what is owed (what it owes) to the Utility and what to the other neighbours
 // 4) calculate weighted averages and based on the pricing calculate what each party owes or is owed
 // 5) return a map of Party - NettingResult
-    TODO("not implemented")
+//    TODO("not implemented")
+    return mapOf(
+            utilityCompany to NettingResult(amountToReceive = 10),
+            "Alice" to NettingResult(amountToPay = 20)
+    )
+}
+
+
+fun main(args: Array<String>) {
+
+    val readings = mapOf("Alice" to 400, "Bob" to -300, "Charlie" to 1000, "John" to -200)
+
+    val result = netting(readings, "MegaUtilityCompany")
+
+    println(result)
+
 }
